cbb35133a6fa259b20ead0ae77edb48fed73f1b4
Gabriel Del <gabrieldelc.barros@gmail.com>
2021-05-21 18:17:02 -0300

    2021-05-21=18:17:02

--- .config/autostart/conky.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# set -x
-killall conky
-sleep 1
-conky -q -c ~/.config/conky/right &
-conky -q -c ~/.config/conky/left & exit
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+#set -x
 if test -f ~/.aliases
     replay source ~/.aliases replay
     set -x PATH (echo $path | sed '/^#/d' | tr '\n' ' ' | string split ' ')
