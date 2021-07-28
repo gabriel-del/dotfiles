2804f67681ab83ed48178351ea866c95854394b3
Gabriel Del <gabrieldelc.barros@gmail.com>
2021-07-28 02:15:14 -0300

    2021-07-28=02:15:14

--- .bin/volume.sh
+++ .bin/volume.sh
@@ -2,7 +2,8 @@
 # USAGE: ./volume.sh up/down/toggle
 step=2
 program=pactl
-theme=/usr/share/icons/Adwaita/16x16/status
+theme=breeze-dark
+theme_path=/usr/share/icons/$theme/status/16
 
 case $program in
   pactl)
@@ -10,7 +11,7 @@ case $program in
     case $1 in
       up) [[ $(volume) -lt 160 ]] && pactl set-sink-volume @DEFAULT_SINK@ +2% ;;
       down) pactl set-sink-volume @DEFAULT_SINK@ -2% ;;
-      toggle) pactl set-sink-volume @DEFAULT_SINK@ toggle ;;
+      mute) pactl set-sink-volume @DEFAULT_SINK@ toggle ;;
       set) pactl set-sink-volume @DEFAULT_SINK@ "$2"% ;;
     esac ;;
   amixer)
@@ -18,24 +19,39 @@ case $program in
     case $1 in
       up) [[ $(volume) -lt 160 ]] && amixer set Master $step%+ ;;
       down) amixer set Master $step%- ;;
-      toggle) amixer set Master toggle ;;
+      mute) amixer set Master toggle ;;
       set) amixer set 'Master' "$2"% ;;
     esac ;;
 esac
 
-if [[ $(volume) -gt 150 ]] ; then
-icon=audio-volume-overamplified-symbolic.symbolic.png
-elif [[ $(volume) -gt 100 ]] ; then
-icon=audio-volume-high-symbolic.symbolic.png
-elif [[ $(volume) -gt 50 ]] ; then
-icon=audio-volume-medium-symbolic.symbolic.png
-elif [[ $(volume) -gt 0 ]] ; then
-icon=audio-volume-low-symbolic.symbolic.png
-else
-icon=audio-volume-muted-symbolic.symbolic.png
+if [[ $theme == breeze-dark ]] ; then
+	if [[ $(volume) -gt 150 ]] ; then
+	icon=audio-on.svg
+	elif [[ $(volume) -gt 100 ]] ; then
+	icon=audio-volume-high.svg
+	elif [[ $(volume) -gt 50 ]] ; then
+	icon=audio-volume-medium.svg
+	elif [[ $(volume) -gt 0 ]] ; then
+	icon=audio-volume-low.svg
+	else
+	icon=audio-volume-muted.svg
+	fi
+elif [[ $theme == Adwaita ]] ; then
+	if [[ $(volume) -gt 150 ]] ; then
+	icon=audio-volume-overamplified-symbolic.symbolic.png
+	elif [[ $(volume) -gt 100 ]] ; then
+	icon=audio-volume-high-symbolic.symbolic.png
+	elif [[ $(volume) -gt 50 ]] ; then
+	icon=audio-volume-medium-symbolic.symbolic.png
+	elif [[ $(volume) -gt 0 ]] ; then
+	icon=audio-volume-low-symbolic.symbolic.png
+	else
+	icon=audio-volume-muted-symbolic.symbolic.png
+	fi
 fi
 
+
 bar=$(seq -s "─" $(($(volume) / 5)) | sed 's/[0-9]//g')
-dunstify -t 2000 -i "$theme/$icon"  -r 2593 -u normal "$(volume)    $bar"
+dunstify -t 2000 -i "$theme_path/$icon"  -r 2593 -u normal "$(volume)    $bar"
 
 
