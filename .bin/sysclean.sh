743ca1d0c8f31f869b926edd424770bee849b610
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-03-04 13:43:54 -0300

    backup all files and create .gitignore

--- .bin/sysclean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pacman -Rscn $(pacman -Qtdq)
-pkgfile -u
-updatedb
-pacman-db-upgrade
-yes | pacman -Scc
-sync
--- .config/conky/conky.sh
+++ .config/conky/conky.sh
@@ -2,5 +2,8 @@
 # set -x
 killall -q conky
 sleep 1
+
+trap "kill 0" EXIT
 conky -q -c ~/.config/conky/right &
-conky -q -c ~/.config/conky/left & exit
+conky -q -c ~/.config/conky/left & 
+wait
